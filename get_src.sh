4037213e52214444b9b4e5a18e1a2035fa72602d
zabooh <martin_ruppert@gmx.de>
2025-08-25 22:18:48 +0200

    copy scripts added

---
+++ get_src.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SRC_DIR="../bsp_patch/mscc-brsdk-source-2024.09/output/mybuild/build/linux-custom/drivers/net/ethernet/microchip/lan865x"
+DST_DIR="src"
+
+
+FILES=(
+    lan865x.c
+    lan865x_ptp.c
+    lan865x_ptp.h
+    Makefile
+    microchip_t1s.c
+    oa_tc6.c
+    oa_tc6.h
+)
+
+for file in "${FILES[@]}"; do
+    echo "Copying $file from $SRC_DIR to $DST_DIR"
+    cp "$SRC_DIR/$file" "$DST_DIR/"
+done
