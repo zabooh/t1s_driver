9bb2d0d13de6594177e02840ffe7592e780a5ef0
zabooh <martin_ruppert@gmx.de>
2025-08-27 14:45:34 +0200

    path changes

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@ ifndef KDIR
 endif
 
 obj-m += microchip_t1s.o
-microchip_t1s-y := src/microchip_t1s.o
+microchip_t1s-y := lan865x/microchip_t1s.o
 obj-m += lan865x_t1s.o
-lan865x_t1s-y := src/lan865x.o src/oa_tc6.o src/lan865x_ptp.o
+lan865x_t1s-y := lan865x/lan865x.o lan865x/oa_tc6.o lan865x/lan865x_ptp.o
 
 all:
 	$(MAKE) -C $(KDIR) M=$(PWD) modules
--- get_src.sh
+++ get_src.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-SRC_DIR="../bsp_patch/mscc-brsdk-source-2024.09/output/mybuild/build/linux-custom/drivers/net/ethernet/microchip/lan865x"
-DST_DIR="src"
+SRC_DIR1="../bsp_patch/mscc-brsdk-source-2024.09/output/mybuild/build/linux-custom/drivers/net/ethernet/microchip/lan865x"
+DST_DIR1="src"
 
-
-FILES=(
+FILES1=(
     lan865x.c
+    lan865x.h
     lan865x_ptp.c
     lan865x_ptp.h
-    Makefile
     microchip_t1s.c
     oa_tc6.c
     oa_tc6.h
 )
 
-for file in "${FILES[@]}"; do
-    echo "Copying $file from $SRC_DIR to $DST_DIR"
-    cp "$SRC_DIR/$file" "$DST_DIR/"
+for file in "${FILES1[@]}"; do
+    echo "Copying $file from $SRC_DIR1 to $DST_DIR1"
+    cp "$SRC_DIR1/$file" "$DST_DIR1/"
 done
+
+
+SRC_DIR2="../bsp_patch/mscc-brsdk-source-2024.09/output/mybuild/build/linux-custom/drivers/net/ethernet/microchip"
+DST_DIR2="."
+
+FILES2=(
+    driver.md
+    load.sh
+    Makefile
+    ptp_todo.md
+    README.md
+    release_notes.md
+)
+
+for file in "${FILES2[@]}"; do
+    echo "Copying $file from $SRC_DIR2 to $DST_DIR2"
+    cp "$SRC_DIR2/$file" "$DST_DIR2/"
+done
+
